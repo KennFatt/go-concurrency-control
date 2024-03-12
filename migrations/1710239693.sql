e859dd7845920b83183d17f9813683aa158b4ae0
Kennan Fattahillah <kennfatt@gmail.com>
2024-03-12 20:59:20 +0700

    chore(migrations): update schema

--- migrations/1710239693.sql
+++ migrations/1710239693.sql
@@ -1,1 +1,2 @@
-CREATE TABLE `seats` (`id` bigint NOT NULL AUTO_INCREMENT, `is_booked` bool NOT NULL DEFAULT false, `passanger_name` varchar(255) NULL, `version` bigint unsigned NULL DEFAULT 0, PRIMARY KEY (`id`)) CHARSET utf8mb4 COLLATE utf8mb4_bin;
+-- Active: 1679834974662@@127.0.0.1@3306@flights
+CREATE TABLE `seats` (`id` bigint NOT NULL AUTO_INCREMENT, `is_booked` bool NOT NULL DEFAULT false, `passenger_name` varchar(255) NULL, `version` bigint unsigned NULL DEFAULT 0, PRIMARY KEY (`id`)) CHARSET utf8mb4 COLLATE utf8mb4_bin;
